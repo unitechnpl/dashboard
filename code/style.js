bcca2f406c1ad8c6fb2a37de742762bf04ebcd8d
kriss malla <86618034+urGODBOY@users.noreply.github.com>
2025-01-25 04:03:53 +0545

    Update style.js

---
+++ code/style.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const profileImg = document.getElementById('profile-img');
+    profileImg.addEventListener('click', () => {
+        alert('Hello, I am Krish Mall!');
+    });
+});
